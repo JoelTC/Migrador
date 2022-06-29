c6a329757597a1158b1251824c3bfc5e8230bcca
Joel Angel Trujillo Cruz <jtrujillo@novatronic.com>
2022-06-28 20:33:16 -0500

    File service

--- GT-web/src/app/app.component.ts
+++ GT-web/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DocAplicacion } from './models/esquema/DocAplicacion';
 import { AplicacionService } from './services/aplicacion/aplicacion.service';
+import { FileService } from './services/file.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,33 @@ import { AplicacionService } from './services/aplicacion/aplicacion.service';
 export class AppComponent {
   title = 'GT-web';
   public docAplacion: DocAplicacion[];
+  fileToUpload: File;
 
-  constructor(private serviceAplicacion: AplicacionService) {
+  constructor(private serviceAplicacion: AplicacionService, private serviceFile: FileService) {
   }
 
   ngOnInit() {
-    this.migrarAplicacion();
+    //this.migrarAplicacion();
+  }
+
+  handleFileInput(event: Event) {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    if (fileList) {
+      console.log("FileUpload -> files", fileList);
+      console.log("FileUpload -> files", fileList[0]);
+
+      this.serviceFile.uploadFile(fileList[0], 'C:\\Users\\jtrujillo\\Desktop\\templates\\templates').subscribe({
+        next: (result: any) => {
+          console.log("Mensaje: ", result);
+        },
+        error: (error) => { "Error: " + console.log(error) }
+      })
+    }
+  }
+
+  abrirReplica(){
+    
   }
 
   public migrarAplicacion() {
---
+++ GT-webcore/src/test/java/com/novatronic/pscabas/gt/webcore/controller/FileControllerTest.java
@@ -0,0 +1,54 @@
+package com.novatronic.pscabas.gt.webcore.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+
+import com.novatronic.pscabas.gt.webcore.domains.responses.Response;
+import com.novatronic.pscabas.gt.webcore.exceptios.MigradorException;
+import com.novatronic.pscabas.gt.webcore.services.interfaces.FileService;
+
+public class FileControllerTest {
+	private static final String PATH = "C:\\Users\\jtrujillo\\Desktop\\templates\\templates";
+	private MockMultipartFile avatar = null;
+
+	private static final String SUCCESS_STATUS = "Success";
+	private static final String SUCCESS_CODE = "200 OK";
+	private static final String SUCCESS_MESSAGE = "OK";
+	private static final String ESTREG = "Los archivos fueron cargados correctamente al servidor";
+
+	@Mock
+	FileService service;
+
+	@InjectMocks
+	FileController controller;
+
+	@Before
+	public void init() throws MigradorException, IOException {
+		MockitoAnnotations.initMocks(this);
+
+		final InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.png");
+		avatar = new MockMultipartFile("file", "test.png", "image/png", inputStream);
+
+		Mockito.when(service.save(avatar, PATH)).thenReturn(ESTREG);
+	}
+
+	@Test
+	public void uploadFiles() throws MigradorException {
+		final Response<String> response = controller.uploadFiles(avatar, PATH);
+		
+		assertEquals(response.getStatuts(), SUCCESS_STATUS);
+		assertEquals(response.getCode(), SUCCESS_CODE);
+		assertEquals(response.getMessage(), SUCCESS_MESSAGE);
+		assertEquals(response.getData(), ESTREG);
+	}
+}
